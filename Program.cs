128edf008aa63c2d75f8136746202136daa24c50
ahmedshady380 <121251100+ahmedshady380@users.noreply.github.com>
2022-12-22 19:25:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Prime_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("The value n1: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.Write("The value n2: ");
+            int n2 = int.Parse(Console.ReadLine());
+
+            for (int x = n1; x <= n2; x++)
+            {
+                int z = 0;
+                for (int y = 1; y <= x; y++)
+                {
+                    if (x % y == 0)
+                    {
+                        z++;
+                    }
+                }
+                if (z == 2)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+
+        }
+    }
+}
---
+++ perfect numbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.Write(" Enter The Value n1 : ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.Write(" Enter The Value n2 : ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        for (int x = n1; x <= n2; x++)
+        {
+            int z = 1;
+            for (int y = 2; y < x; y++)
+            {
+                if (x % y == 0)
+                {
+                    z += y;
+                }
+            }
+            if ((z == x) && (z != 1))
+            {
+                Console.WriteLine(x);
+            }
+        }
+
+
+
+
+    }
+}
